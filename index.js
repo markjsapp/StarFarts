df7aca9ac31938852163f7bd12104d77a1de33e2
Gabe Medina <markjsapp@gmail.com>
2024-02-27 22:29:16 -0500

    randomizing results

--- index.js
+++ index.js
@@ -4,15 +4,27 @@ const expect = require('expect.js');
 axios.get('https://swapi.dev/api/starships/')
   .then(response => {
     // Log the response data to see the API response
-    console.log(response.data.results[0]); 
+    const allStarshipData = response.data.results;
+
+    const randomStarship = getRandomElement(allStarshipData); 
+    
+    console.log(randomStarship);
+    console.log('name:' + randomStarship.name);
 
     // Use expect.js to assert the status code
     expect(response.status).to.be(200);
 
     // Let the user know that the call was successful
-    console.log("API call was successful with status 200");
+    console.log("\nAPI call was successful with status 200");
   })
   .catch(error => {
     // Let the user know some bad stuff just happened
     console.error("API call failed", error);
   });
+
+
+
+function getRandomElement(array) {
+  const randomIndex = Math.floor(Math.random() * array.length);
+  return array[randomIndex];
+}
